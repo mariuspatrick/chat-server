b33e557fd3e13edd011ed56b4c527d9c0bb39882
mariuspatrick <marius.patrick72@gmail.com>
2019-12-13 11:02:50 +0100

    Added async functions

--- message/router.js
+++ message/router.js
@@ -6,21 +6,19 @@ const { Router } = express
 
 const router = new Router()
 
-router.get("/message", (req, res, next) => {
-		Message.findAll()
-		.then(messages => {
-				res.send(messages)
-			})
-			.catch(next)
+router.get("/message", async (req, res, next) => {
+	const messages = await Message 
+		.findAll()
+		res.send(messages)
+		//	.catch(next)
 	}
 )
 
-router.post("/message", (req, res, next) => {
-		Message.create(req.body)
-		.then(message => {
-			res.send(message)
-		})
-		.catch(next)
+router.post("/message", async (req, res, next) => {
+		const message = await Message
+		.create(req.body)
+		res.send(message)
+		// .catch(next)
 	}
 )
 
